42f5764006d3d04fd25e352092fcedacbd9a0138
lebien <lehuybien@gmail.com>
2020-07-05 14:35:27 +0700

    édds

---
+++ eCommerce-Bookstore-master/eCommerce-Bookstore-master/src/test/java/com/lan/bookstore/controller/customerController/HomeControllerTest.java
@@ -0,0 +1,38 @@
+package com.lan.bookstore.controller.customerController;
+
+/**
+ *
+ */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"/config/application-context.xml"})
+public class HomeControllerTest {
+  //  private final String USER_AGENT = "Mozilla/5.0";
+
+//    @Test
+//    public void testFaq() throws IOException {
+//        String url = "http://localhost:8080/faq";
+//        URL obj = new URL(url);
+//        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+//
+//        con.setRequestMethod("POST");
+//        con.setRequestProperty("User-Agent", USER_AGENT);
+//
+//        int responseCode = con.getResponseCode();
+//        System.out.println("\nSending 'POST' request to URL : " + url);
+//        System.out.println("Response Code: " + responseCode);
+//
+//        BufferedReader in = new BufferedReader(
+//                new InputStreamReader(con.getInputStream()));
+//        String inputLine;
+//        StringBuffer response = new StringBuffer();
+//
+//        while ((inputLine = in.readLine()) != null) {
+//            response.append(inputLine);
+//        }
+//        in.close();
+//
+//        //print result
+//        System.out.println(response.toString());
+//
+//    }
+}
